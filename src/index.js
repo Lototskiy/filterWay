c6c43ef4fc4bad99451e9fe2a54321b34e865fb6
Dima <timoha.lisyev2014@yandex.ru>
2021-07-14 13:14:39 +0300

    add catalog-test page

--- src/index.js
+++ src/index.js
@@ -37,6 +37,9 @@ import './assets/css/blocks/catalog-card.scss';
 import './assets/css/blocks/nothing-hide.scss';
 import './assets/css/blocks/subscription.scss';
 import './assets/css/blocks/pagination.scss';
+import './assets/css/blocks/brand.scss';
+import './assets/css/blocks/standarts.scss';
+import './assets/css/blocks/catalog-test.scss';
 
 
 
