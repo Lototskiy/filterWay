29f66732f82a793a84c1e28c11a908f4c8199df7
Vls <bestblodd@gmail.com>
2021-07-20 16:25:09 +0300

    fix catalog

--- src/assets/js/catalog.js
+++ src/assets/js/catalog.js
@@ -1,11 +1,11 @@
-let linkMap = document.querySelector('.catalog__row-filter');
-let map = document.querySelector('.catalog__sidebar');
-let closeMap = document.querySelector('.catalog__sidebar-close');
+let linkMap = $('.catalog__row-filter');
+let map = $('.catalog__sidebar');
+let closeMap = $('.catalog__sidebar-close');
 
 
-linkMap.addEventListener('click', function () {
+linkMap.click( function () {
     map.classList.add('catalog__sidebar--active');
 })
-closeMap.addEventListener('click', function () {
+closeMap.click(function () {
     map.classList.remove('catalog__sidebar--active');
 })
