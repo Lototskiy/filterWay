c37703c62e6abf72bd22bd72e998aaf369ffa43b
Vls <bestblodd@gmail.com>
2021-07-22 16:02:49 +0300

    end cabinet

--- src/assets/js/toggles-tab.js
+++ src/assets/js/toggles-tab.js
@@ -2,18 +2,19 @@ $('.toggle').click(function (e) {
     e.preventDefault();
     
     $('.pay-tabs').find('.radio__input').attr('checked', false)
-    $('.toggle').removeClass('toggle--active')
+    $('.toggle').parent().removeClass('list-active')
     $(this).find('.radio__input').attr('checked', true)
-    $(this).addClass('toggle--active')
+    $(this).parent().addClass('list-active')
 
 
     var $this = $(this);
 
-    if ($this.hasClass('toggle--active')) {
-        $this.removeClass('toggle--active')
-    } else {
-        $this.addClass('toggle--active')
-    }
+    // if ($this.closest('li').hasClass('list-active')) {
+    //     $this.removeClass('list-active')
+    //     console.log($this)
+    // } else {
+    //     $this.addClass('list-active')
+    // }
 
     if ($this.next().hasClass('show')) {
         $this.next().removeClass('show');
@@ -24,4 +25,5 @@ $('.toggle').click(function (e) {
         $this.next().toggleClass('show');
         $this.next().slideToggle(350);
     }
+
 });
