c3cc15d6e4d4529faa98e425cef9ae8474765c16
Vls <bestblodd@gmail.com>
2021-07-22 14:30:28 +0300

    fix adaptiv

--- src/assets/js/toggles-tab.js
+++ src/assets/js/toggles-tab.js
@@ -1,10 +1,19 @@
 $('.toggle').click(function (e) {
     e.preventDefault();
+    
     $('.pay-tabs').find('.radio__input').attr('checked', false)
+    $('.toggle').removeClass('toggle--active')
     $(this).find('.radio__input').attr('checked', true)
+    $(this).addClass('toggle--active')
 
 
     var $this = $(this);
+
+    if ($this.hasClass('toggle--active')) {
+        $this.removeClass('toggle--active')
+    } else {
+        $this.addClass('toggle--active')
+    }
 
     if ($this.next().hasClass('show')) {
         $this.next().removeClass('show');
